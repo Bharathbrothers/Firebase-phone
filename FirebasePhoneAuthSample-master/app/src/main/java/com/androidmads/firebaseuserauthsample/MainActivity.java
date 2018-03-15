6fa1d916ce2e0fbd3663a62d421fffe8396323ff
Bharath <sathuribharathbrothers1234@gmail.com>
2018-03-15 20:11:02 +0530

    files

---
+++ FirebasePhoneAuthSample-master/app/src/main/java/com/androidmads/firebaseuserauthsample/MainActivity.java
@@ -0,0 +1,74 @@
+package com.androidmads.firebaseuserauthsample;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements
+        View.OnClickListener {
+
+    private static final String TAG = "PhoneAuthActivity";
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button mSignOutButton = (Button) findViewById(R.id.sign_out_button);
+        TextView fireBaseId = (TextView) findViewById(R.id.detail);
+        //Button web = (Button)findViewById(R.id.button2);
+        mAuth = FirebaseAuth.getInstance();
+        if(mAuth!=null){
+            fireBaseId.setText(mAuth.getCurrentUser().getPhoneNumber());
+        }
+        mSignOutButton.setOnClickListener(this);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View view) {
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:9866864617"));
+                if (PackageManager.PERMISSION_GRANTED != ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.CALL_PHONE)) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+
+
+                    return;
+                }
+                startActivity(callIntent);
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.sign_out_button:
+                mAuth.signOut();
+                startActivity(new Intent(MainActivity.this, PhoneAuthActivity.class));
+                finish();
+                break;
+
+        }
+    }
+}
